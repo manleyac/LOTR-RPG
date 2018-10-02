6f6d69c700f8d6dfd34a16f16fc76e1d0a625449
Andrew <8879789+manleyac@users.noreply.github.com>
2018-10-01 22:17:11 -0400

    select player and enemy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,3 +13,37 @@ class Hero {
    }
    
 }
+function selectPlayer(event) {
+   $("#player").append(this);
+   $(this).removeClass(".hero");
+   $(".hero").off();
+   selectEnemy();
+}
+
+function selectEnemy() {
+   $("#message").text("Select an Enemy to face");
+   $(".hero").on("click", function(event) {
+      $("#enemy").append(this);
+      $(".hero").off();
+      addAttack();
+   });
+   
+}
+
+function addAttack() {
+   var button = $("<button>Attack</button>");
+   $("#player").append(button);
+}
+
+function main() {
+   var Aragorn = new Hero(100,20,15);
+   var Gandalf = new Hero(150,15,20);
+   var Saruman = new Hero(85,25,20);
+   $("#message").text("Select a Player by clicking any Character");
+   $(".hero").on("click", selectPlayer);
+}
+
+main();
+
+
+
